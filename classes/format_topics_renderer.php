b458b5b42aae8421d9e373895da65df3f6d3cef8
Hipjea <pierre.duverneix@gmail.com>
2023-10-05 10:42:03 +0200

    format_topics_renderer

--- classes/format_topics_renderer.php
+++ classes/format_topics_renderer.php
@@ -2,11 +2,9 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-require_once($CFG->dirroot . "/course/format/topics/renderer.php");
+use format_topics\output\renderer;
 
 class theme_luniversitenumerique_format_topics_renderer extends format_topics_renderer {
-    
-
     /**
      * Generate a summary of a section for display on the 'course index page'
      *
