7f666e0293ab502d57533813492441568a90c088
Hipjea <pierre.duverneix@gmail.com>
2023-10-05 10:40:27 +0200

    remove addblockposition

--- config.php
+++ config.php
@@ -34,4 +34,3 @@ $THEME->enable_dock = false;
 $THEME->yuicssmodules = array();
 $THEME->rendererfactory = 'theme_overridden_renderer_factory';
 $THEME->requiredblocks = '';
-$THEME->addblockposition = BLOCK_ADDBLOCK_POSITION_FLATNAV;
